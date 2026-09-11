d0d919e233caf21868f81c99ecc9f3fd9c346fb3
abhimanyuvigyani <abhimanyuvigyani@gmail.com>
2026-09-11 23:22:57 +0530

    Create Que_3.cs

---
+++ Que_3.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Que_3
+{
+    delegate void Sir_ye_3rd_assignment_hai_ya_4th();
+
+    static void Method1()
+    {
+        Console.WriteLine("Ye hai pahla pahla method 🎵");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("ye hai method 2 obviously");
+    }
+
+    public static void Main1()
+    {
+        Sir_ye_3rd_assignment_hai_ya_4th d1 = Method1;
+
+        Console.WriteLine("Unicast Delegate:");
+        d1();
+
+        Sir_ye_3rd_assignment_hai_ya_4th d2 = Method1;
+        d2 += Method2;
+
+        Console.WriteLine("\nMulticast Delegate:");
+        d2();
+    }
+}
